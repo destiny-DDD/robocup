1ab5244605eae364ae2db138e1b408dee47f9c83
destiny <2303306754@qq.com>
2026-08-23 01:33:35 +0800

    读取相机数据完成

--- src/myvideo/include/myvideo/myvideo.hpp
+++ src/myvideo/include/myvideo/myvideo.hpp
@@ -2,14 +2,20 @@
 #define __MYVIDEO_HPP_
 
 #include "rclcpp/rclcpp.hpp"
+#include <opencv2/opencv.hpp>
 
 namespace myvideo {
 
 class MyVideo : public rclcpp::Node {
 public:
   MyVideo(const std::string &name);
+  void TimeCallback();
 
 private:
+  cv::VideoCapture cap;
+  cv::Mat image_origin;
+
+  rclcpp::TimerBase::SharedPtr timer_;
 };
 
 } // namespace myvideo
--- src/myvideo/src/myvideo.cpp
+++ src/myvideo/src/myvideo.cpp
@@ -1,22 +1,22 @@
 #include "myvideo.hpp"
-
-#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
 
 namespace myvideo {
-MyVideo::MyVideo(const std::string &name):Node(name){
-    cv::VideoCapture cap(0);
-    while(1)
-    {
-        cv::Mat frame;
-        cap>>frame;
-        cv::imshow("img",frame);
-        if(cv::waitKey(10)==27)
-            break;
-    }
-}
-}
+MyVideo::MyVideo(const std::string &name) : Node(name) {
+  cap.open(0);
+  if (!cap.isOpened()) {
+    RCLCPP_ERROR(this->get_logger(), "无法打开摄像头 /dev/video0");
+    return;
+  }
 
-int main(int argc, char **argv)
-{
-
+  timer_ = this->create_wall_timer(std::chrono::milliseconds(30),
+                                   [this]() { TimeCallback(); });
 }
+void MyVideo::TimeCallback() {
+  cap.read(image_origin);
+  cv::imshow("img", image_origin);
+  cv::waitKey(1);
+}
+} // namespace myvideo
+
+int main(int argc, char **argv) {}
